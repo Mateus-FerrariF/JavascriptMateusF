06b6ea01e1461bca36a966beea2d013d3c973ef2
Mateus-FerrariF <mateusferrarifeliciano@gmail.com>
2025-02-19 20:37:22 -0300

    save

---
+++ aulas/exercicio 3.js
@@ -0,0 +1,7 @@
+var RandInt = Math.floor(Math.random()*100); // int aleatorio de 0~99 (Math.floor = int) (Math.random = 0~1 float)
+var skulname = "sepsers"; // string
+var skull = 42378; // int
+var skulles = 87.55; // float
+
+console.log(`Hello Player "${skulname}" \nLevel ${skulles} \nPontos ${skull} \nRandomInt ${RandInt}`); // (console.log = Literalmente oq tá escrito)(\n = linebreak)
+
---
+++ aulas/exercicio 4.js
@@ -0,0 +1,15 @@
+// variables
+var y = 9
+var x = 4
+
+var sum = x+y
+var sbt = x-y
+var div = x/y
+var mult = x*y
+var exp = x**y
+var sqrx = Math.sqrt(x)
+var sqry = Math.sqrt(y)
+
+console.log("Soma: ",sum,"\n","Subtração: ",sbt,"\n","Divisão: ",div,"\n","Multiplicação: ","\n",)
+console.log("\n", sqrx, sqry )
+
