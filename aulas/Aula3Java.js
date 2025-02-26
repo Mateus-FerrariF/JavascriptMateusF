f96ceba5e18796a086a5f87fd802934c2252e3e6
Mateus-FerrariF <mateusferrarifeliciano@gmail.com>
2025-02-26 20:38:46 -0300

    Se2

---
+++ aulas/Aula3Java.js
@@ -0,0 +1,17 @@
+// se a I é 16~17 ou 70~ Opcional o voto, I<16 = não vota, 
+
+var a = 5;
+
+console.log("Pré-incremento", ++a)
+console.log("Pré-incremento", ++a)
+console.log("Pós-incremento", a++)
+console.log("Pós-decremento", a--)
+console.log("Pós-decremento", --a)
+
+console.log(5==5) // x é igual a x!
+console.log("5"==5) // não chegou a tipagem
+console.log("5"===5) // checou a tipagem
+console.log("5"!=5) // não checou a tipagem; false
+console.log("5"!==5) // checou a tipagem; true
+
+
--- aulas/Exercicio 5.js
+++ aulas/Exercicio 5.js
@@ -1,1 +1,15 @@
-// se a I é 16~17 ou 70~ Opcional o voto, I<16 = não vota, 
+
+var I = 1000;
+
+if (I >= 200) {
+    console.log("IMORTAL");
+}
+else if (I >= 18) {
+    console.log("maior de idade");
+}
+if (I <= 0) {
+    console.log("VOCÊ NÃO EXISTE");
+}
+else if (I <= 17){
+    console.log("menor de idade");
+}
