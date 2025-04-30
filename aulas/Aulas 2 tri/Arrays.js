e068d2209a99d5293f44a4137a18f4331085ba3e
Mateus-FerrariF <mateusferrarifeliciano@gmail.com>
2025-04-30 20:01:48 -0300

    ss

---
+++ aulas/Aulas 2 tri/Arrays.js
@@ -0,0 +1,13 @@
+
+//     [pos0, pos1, pos2]
+/*
+var num = [1,2,3];
+var k = 0;
+console.log(num.at(2)); // loga o numero em pos2; 3.
+console.log("Num pos2 * 5: ", num.at(2)*5);
+*/
+
+
+// posições:    p0    p1     p2   p3   p4
+var pessoa = ['Mit','Kilt','Oak', 69, 8008];
+console.log(pessoa);
---
+++ aulas/Aulas 2 tri/aula 1.js
@@ -0,0 +1,30 @@
+
+//loga os numeros 0<5; (0, 1, 2, 3, 4) 
+/*
+for(var i=0;i<5;i++){
+    console.log(i);
+};
+*/
+
+// while: verifica e depois executa
+/*
+var car = 0;
+var execuções = 0;
+
+while (car<5){
+    console.log(car);
+    execuções++;
+    car = car+(Math.random()); // <--- MATH RANDOOOOOM Para arredondar para inteiro: Math.floor(numero/codigo/*função (não tenho certeza))
+};
+
+console.log("\nExecuções: ",execuções);
+*/
+
+// do while: executa e depois verifica
+/*
+var carro = 0;
+do{
+    console.log(carro);
+    carro++;
+} while(carro<10);
+*/
