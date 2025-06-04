809a32a69480e927e63c42c8217623785a56b41b
Mateus-FerrariF <mateusferrarifeliciano@gmail.com>
2025-06-04 20:31:04 -0300

    ss

---
+++ aulas/Aulas 2 tri/Atividade0406/Exercicios0406.js
@@ -0,0 +1,32 @@
+//Exercicio 1
+/*
+function div(x) {
+    if ()
+}
+
+
+*/
+
+
+
+//Exercicio 3
+/*
+function tab(x) {
+    for(let i=0;i<=10;i++){
+        res = i*x;
+        console.log(x,"*",i,"=", res);
+    };
+}
+tab(5)
+*/
+
+//Exercicio 4
+
+function contagem() {
+    var cont = 0;
+    while (cont<100) {
+        cont=cont+1;
+        console.log(cont);
+    };
+};
+contagem();
---
+++ aulas/Aulas 2 tri/func2.js
@@ -0,0 +1,74 @@
+// função com decisão simples if/else
+/*
+function podvotar(idade) {
+    if (idade>=18) {
+        console.log("pode votar")
+    } 
+    else {
+        console.log("não pode votar")
+    }
+}
+podvotar(19)
+*/
+
+// --------------------------------------------------------------------------------
+
+
+// if/else if/else
+/*
+function verificarNumero(num){
+    if (num>0) {
+        console.log("é positivo");
+    }
+    else if (num < 0) {
+        console.log("é negativo");
+    }
+    else {
+        console.log("ZEROOOO!");
+    };
+};
+verificarNumero(-10)
+*/
+
+// --------------------------------------------------------------------------------
+
+
+// função com for
+/*
+function printNum(){
+    for (let i=1; i<=10; i++){
+        console.log(i);
+    }
+};
+printNum();
+*/
+
+// --------------------------------------------------------------------------------
+
+
+// Função com while
+/*
+function printwhile(){
+    var i = 0;
+    while (i<=10){
+        console.log(i);
+        i++;
+    };
+};
+printwhile();
+*/
+
+// --------------------------------------------------------------------------------
+
+
+//Função com do
+
+function printDo(){
+    var i = 1;
+    do {
+        console.log(i);
+        i++;
+    } while (i<=10);
+};
+
+printDo();
